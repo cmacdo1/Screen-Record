2c4dbab89bdd1f6801cb7118a3d8a2a98a63af37
cmacdo1 <chrismacdonaldphotography@yahoo.com>
2025-08-03 15:30:49 -0700

    App creation and setup

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div className='flex flex-col items-center justify-center h-screen bg-gray-25'>
+      <h1 className='text-2xl font-bold underline pb-4'>Screen Record</h1>
+      <p>This is the screen record page.</p>
+    </div>
+  )
+}
+
+export default Page
